354d19fce98f6e8de88128cb87b3f70169e16b04
Delion <21adtydwf@gmail.com>
2025-10-20 23:08:54 +0700

    update: membuat controller login dan template view

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     private $user = [
-    ]
+        ['username' => 'naveria', 'password' => 'naver12'],
+        ['username' => 'liliaveria', 'password => lixx19']
+    ];
+
+    private $tabungan = [
+        ['nama' => 'Naveria', 'saldo' => '9000000'],
+        ['nama' => 'Liliaveria', 'saldo' => '6000000']
+    ];
+
+    public function login() {
+        return view('login');
+    }
 }
