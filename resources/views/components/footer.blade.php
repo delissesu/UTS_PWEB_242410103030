bb037ef47c9c79e2d53fbec84c34a48feabbf1bd
Delion <21adtydwf@gmail.com>
2025-10-20 23:27:16 +0700

    add footer component with current year display

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,11 @@
 <div>
     <!-- When there is no desire, all things are at peace. - Laozi -->
 </div>
+
+{{-- Gini dulu --}}
+<footer style="text-align: center; margin-top:40px; color:#777;">
+    <hr>
+    <p>
+        {{ date('Y') }} Naveria Saving App
+    </p>
+</footer>
