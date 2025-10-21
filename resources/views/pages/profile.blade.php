18d717e293c7d52f4f5b3c46d3b38ca8652efec0
Delion <21adtydwf@gmail.com>
2025-10-21 08:50:22 +0700

    update: struktur view sederhana buat profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,12 +1,22 @@
 @extends('layouts.app');
 @section('title', 'profile');
 
-@section('content')
-
 {{-- <div>
     <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
 </div> --}}
-
+@section('content')
 <h1>
-    ini profile 
+    ini profile
 </h1>
+@if($username) {
+    <p>
+        Ini profile gw
+        {{ $username }}.
+    </p>
+}
+@endif
+
+<a href="/dashboard?username={{ urlencode($username) }}">
+    Balik ke beranda
+</a>
+@endsection
