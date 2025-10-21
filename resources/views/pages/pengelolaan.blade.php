bab982024ea5d3571bd187d5dbd50d14135f4286
Delion <21adtydwf@gmail.com>
2025-10-21 08:57:46 +0700

    update: lagi dan lagi view pengelolaan

--- resources/views/pages/pengelolaan.blade.php
+++ resources/views/pages/pengelolaan.blade.php
@@ -1,3 +1,29 @@
 {{-- <div>
     <!-- It always seems impossible until it is done. - Nelson Mandela -->
 </div> --}}
+
+@extends('layouts.app')
+@section('title', 'kelola tabungan')
+
+@section('content')
+<h1>
+    Tes dulu
+</h1>
+
+<table border="1" cellpadding="6">
+    <tr>
+        <th>Hari</th>
+        <th>Nama</th>
+        <th>Saldo</th>
+    </tr>
+
+    @foreach ($data as $i =>$item ) {
+        <tr>
+            <td>{{ $item['hari'] }}</td>
+            <td>{{ $item['nama'] }}</td>
+            <td>{{ number_format($item['saldo'], 0, ',', '.') }}</td>
+        </tr>
+    }
+    @endforeach
+</table>
+@endsection
