a5fe9083914403362a4c313bc5ea24b1f7932a5b
Delion <21adtydwf@gmail.com>
2025-10-21 19:18:37 +0700

    update: mecah route ke beberapa handler

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TabunganController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', [PageController::class, 'login']);
-Route::post('/login', [PageController::class,'handleLogin']);
-Route::get('/dashboard', [PageController::class,'dashboard']);
-Route::get('/profile', [PageController::class,'profile']);
-Route::get('/pengelolaan', [PageController::class,'pengelolaan']);
+Route::get('/', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'handleLogin']);
+Route::get('/logout', [AuthController::class, 'logout']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/profile', [ProfileController::class, 'index']);
+Route::get('/pengelolaan', [TabunganController::class, 'index']);
