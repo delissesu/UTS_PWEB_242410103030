1b36de18543e4ec1dcf90b4340cb43fc748154f5
Delion <21adtydwf@gmail.com>
2025-10-21 08:18:49 +0700

    update: controller handle login

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use function Laravel\Prompts\password;
 
 class PageController extends Controller
@@ -18,19 +17,21 @@ class PageController extends Controller
         ['nama' => 'Liliaveria', 'saldo' => '6000000']
     ];
 
+    // Login return value ke routes
     public function login() {
         return view('login');
     }
 
+    // Login handler
     public function handelLogin(Request $request) {
         $username = $request->input('username');
         $password = $request->input('password');
 
         foreach($this->users as $user) {
             if ($user['username'] == $username && $user['password'] == $password) {
-                return redirect()
+                return redirect('dashboard?=username', urlencode($username));
             }
         }
-
+        return back()->with('error', 'username atau password salah');
     }
 }
